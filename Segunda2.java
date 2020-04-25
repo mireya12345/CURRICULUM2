3d4a3a380ba44bbc33d0da278fef01293bf521a1
mireya12345 <64290380+mireya12345@users.noreply.github.com>
2020-04-24 20:44:45 -0500

    Add files via upload

---
+++ Segunda2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mireya
+ */
+public class Segunda2 {
+    public static void main(String[]args){
+        // Declarar variables numerico y texto
+        int edad; // Declarando
+        edad=25;  // asignando e inicializando
+        char genero ='F';
+        String nombre = "Mireya";
+        boolean estadocivil = false;
+        estadocivil = false;
+        boolean estudia = true;
+        estudia= true;
+        char talla = 'M';
+        String estudios = "Desarrollo de software";
+        System.out.println("Hola, soy "+nombre+" , mi edad es:"+edad+"años ,mi genero es: "+genero+", Estoy casada?: "+estadocivil+" ,Estoy estudiando?: "+ estudia+" ,Que estudio?: "+ estudios" " );
+        
+        
+              
+        
+    }
+}
